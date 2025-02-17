695c2fdbf08cf380f2b987be935fba1506217e3a
michael-bautista <mrbautistadev@gmail.com>
2025-02-17 12:10:08 -0800

    Initial Commit

---
+++ HybridAI/HybridAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HybridAIApp.swift
+//  HybridAI
+//
+//  Created by Michael Bautista on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct HybridAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HybridAIUITests/HybridAIUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  HybridAIUITestsLaunchTests.swift
+//  HybridAIUITests
+//
+//  Created by Michael Bautista on 2/17/25.
+//
+
+import XCTest
+
+final class HybridAIUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
